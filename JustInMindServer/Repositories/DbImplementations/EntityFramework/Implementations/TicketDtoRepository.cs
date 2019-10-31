ca18a54a30c5e0d5d19162108e69141ec1608825
Aldamar <danila.samusev@mail.ru>
2019-10-31 18:37:15 +0300

    Sql query refactor.

--- JustInMindServer/Repositories/DbImplementations/EntityFramework/Implementations/TicketDtoRepository.cs
+++ JustInMindServer/Repositories/DbImplementations/EntityFramework/Implementations/TicketDtoRepository.cs
@@ -31,11 +31,11 @@ namespace JustInMindServer.Repositories.DbImplementations.EntityFramework.Implem
                 "Select t.id, t.name, createdOn, stateId, urgencyId, desiredResolutionDate, u1.firstName as ownerName," +
                 " u2.firstName as approverName, u3.firstName as assigneeName, description, c.name as category " +
             "from tickets t " +
-            "join users u1 on u1.id = 2 " +
-            "join users u2 on u2.id = 2 " +
-            "join users u3 on u3.id = 2 " +
-            "join categories c on c.id = 2 " +
-            "where t.id = 1"
+            "left join users u1 on u1.id = t.ownerId " +
+            "left join users u2 on u2.id = t.approverId " +
+            "left join users u3 on u3.id = t.assigneeId " +
+            "left join categories c on c.id = t.categoryId " +
+            $"where t.id = {ticketId}"
             ).ToList();
 
             return ticketsDto;
